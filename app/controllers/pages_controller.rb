1a13525c730b2c0f5fbac1337dfb8ab0d1202f9e
Marisha Sahay <marishasahay@gmail.com>
2017-03-20 14:24:55 -0700

    Changed timezone to PST

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,8 @@ class PagesController < ApplicationController
   def result
     @date1 = params[:date1]
     @date2 = params[:date2]
-    Time.parse(@date1).in_time_zone('Pacific Time (US & Canada)')
-    Time.parse(@date2).in_time_zone('Pacific Time (US & Canada)')
+    Time.parse(@date1)
+    Time.parse(@date2)
     # @date2 = params[:date2] || Date.today
     @information = Information.where("dist_from_la <= mag * 100 AND (time BETWEEN '#{@date1}' and '#{@date2}')").order("time").limit 10
   end
